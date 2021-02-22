3ecb91c4274f2f21bdcc0e6ec635fa02ace01a36
MineEric64 <besteric40@gmail.com>
2021-02-23 04:48:56 +0900

    롤 실시간 KDA 이벤트 구현
    정글 요네 생각보다 괜찮고 쓸만한 듯?

---
+++ Clients/LolLiveEvent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectReinforced.Clients
+{
+    class LolLiveEvent
+    {
+        //https://developer.riotgames.com/docs/lol#league-client-api
+
+        public string[] Assisters { get; }
+
+        public int EventID { get; }
+        public string EventName { get; }
+        public double EventTime { get; }
+
+        public string KillerName { get; }
+        public string VictimName { get; }
+
+        public static LolLiveEvent Empty =>
+            new LolLiveEvent(new string[] { }, 0, string.Empty, 0.0, string.Empty, string.Empty);
+
+        public LolLiveEvent(string[] assisters, int eventId, string eventName, double eventTime, string killerName, string victimName)
+        {
+            Assisters = assisters;
+
+            EventID = eventId;
+            EventName = eventName;
+            EventTime = eventTime;
+
+            KillerName = killerName;
+            VictimName = victimName;
+        }
+    }
+}
